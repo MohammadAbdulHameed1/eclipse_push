f4f99af3d9289f1add1a1b719bbc782a29e79d00
MohammadAbdulHameed1 <mohammadabdulhameed463@gmail.com>
2026-02-14 14:39:42 +0530

    initial first commit

---
+++ src/main/java/cse_jenkins/push_github/App.java
@@ -0,0 +1,13 @@
+package cse_jenkins.push_github;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
