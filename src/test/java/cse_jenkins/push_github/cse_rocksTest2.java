02dc4dd46e1764d13f1d67e6e842b78e9e515e28
MohammadAbdulHameed1 <mohammadabdulhameed463@gmail.com>
2026-02-14 16:24:20 +0530

    sixth commit

--- src/test/java/cse_jenkins/push_github/cse_rocksTest2.java
+++ src/test/java/cse_jenkins/push_github/cse_rocksTest2.java
@@ -3,13 +3,18 @@ package cse_jenkins.push_github;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class cse_rocksTest2 {
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-    cse_rocks cal = new cse_rocks();
+class CseRocksTest {
+	cse_rocks cal = new cse_rocks();
 
-    @Test
-    void test1() {
-        int result = cal.mul(2, 3);
-        assertEquals(6, result);
-    }
+	@Test
+	void test1() {
+		int result = cal.mul(2, 3);
+		assertEquals(6, result);
+	}
 }
